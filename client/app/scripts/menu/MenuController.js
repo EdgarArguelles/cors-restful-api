18e524d47c136cc452c80d91115aca6d584a367d
Pablo De Nadai <pablodenadai@gmail.com>
2013-07-19 20:49:52 +1000

    Fix Sign out server Url

--- client/app/scripts/menu/MenuController.js
+++ client/app/scripts/menu/MenuController.js
@@ -1,13 +1,13 @@
 'use strict';
 
-app.controller('MenuCtrl', function ($scope, $location, $http, AuthenticationModel) {
+app.controller('MenuCtrl', function ($scope, $location, $http, ServerUrl, AuthenticationModel) {
 
 	$scope.AuthenticationModel = AuthenticationModel;
 
 	$scope.signOut = function () {
 		AuthenticationModel.removeUser();
 		$location.path('/');
-		return $http.get('http://localhost:3000/api/auth/signout');
+		return $http.get(ServerUrl + '/api/auth/signout');
 	};
 
 });

f914683e0ecebdc6db1634c308def914d381d538
Pablo De Nadai <pablodenadai@gmail.com>
2013-07-19 14:51:21 +1000

    Setup Heroku

--- client/heroku/web.js
+++ client/heroku/web.js
@@ -1,6 +1,5 @@
-var gzippo = require('gzippo');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-app.use(gzippo.staticGzip(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
